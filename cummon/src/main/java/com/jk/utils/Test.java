c22276df35f5a325bd277cebe617f4658b0bfa7b
mkj-111 <1816775072@qq.com>
2020-11-01 21:34:25 +0800

    test

---
+++ cummon/src/main/java/com/jk/utils/Test.java
@@ -0,0 +1,11 @@
+package com.jk.utils;
+
+/**
+ * @author 毛康健
+ * @description
+ * @create 2020/11/1
+ */
+public class Test {
+
+
+}
---
+++ cummon/src/main/java/com/jk/utils/TestBean.java
@@ -0,0 +1,16 @@
+package com.jk.utils;/**
+@author 毛康健
+@description
+@create  2020/11/1
+*/public class TestBean {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
